743f905f2a2cb1820bf3423564762f6f35b3b863
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2024-11-07 21:05:32 -1200

    Rename and Upload

---
+++ source/VowelCountApp/Form1.Designer.vb
@@ -0,0 +1,119 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Title = New Label()
+        String_name = New Label()
+        noofvowels = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        SuspendLayout()
+        ' 
+        ' Title
+        ' 
+        Title.AutoSize = True
+        Title.Location = New Point(49, 40)
+        Title.Name = "Title"
+        Title.Size = New Size(183, 15)
+        Title.TabIndex = 0
+        Title.Text = "Program to perform Vowel Count"
+        ' 
+        ' String_name
+        ' 
+        String_name.AutoSize = True
+        String_name.Location = New Point(49, 68)
+        String_name.Name = "String_name"
+        String_name.Size = New Size(91, 15)
+        String_name.TabIndex = 1
+        String_name.Text = "Enter the String:"
+        ' 
+        ' noofvowels
+        ' 
+        noofvowels.AutoSize = True
+        noofvowels.Location = New Point(49, 93)
+        noofvowels.Name = "noofvowels"
+        noofvowels.Size = New Size(107, 15)
+        noofvowels.TabIndex = 2
+        noofvowels.Text = "Number of Vowels:"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(162, 61)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(100, 23)
+        TextBox1.TabIndex = 3
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(162, 90)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(100, 23)
+        TextBox2.TabIndex = 4
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(81, 128)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 5
+        Button1.Text = "Result"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(162, 128)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(75, 23)
+        Button2.TabIndex = 6
+        Button2.Text = "Exit"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(noofvowels)
+        Controls.Add(String_name)
+        Controls.Add(Title)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Title As Label
+    Friend WithEvents String_name As Label
+    Friend WithEvents noofvowels As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+
+End Class
---
+++ source/VowelCountApp/Form1.vb
@@ -0,0 +1,27 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim str1, str2
+        Dim vcount, i, str1len As Integer
+
+        str1 = TextBox1.Text
+        str1len = Len(str1)
+        For i = 1 To str1len
+            str2 = Mid(str1, i, 1)
+            If str2 = "a" Or str2 = "e" Or str2 = "i" Or str2 = "o" Or str2 = "u" Or str2 = "A" Or str2 = "E" Or str2 = "I" Or str2 = "O" Or str2 = "U" Then
+                vcount = vcount + 1
+            End If
+        Next
+        TextBox2.Text = vcount
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Close()
+    End Sub
+End Class
